648b947601169abf52397a5d58705b9da1f53f0a
Zach <zmisson424@gmail.com>
2023-01-12 14:10:18 -0600

    added localizations

--- lib/pages/app/view/app_view.dart
+++ lib/pages/app/view/app_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:squad/pages/home/home.dart';
 import 'package:squad/theme/theme.dart';
 
@@ -22,8 +24,18 @@ class _AppViewState extends State<AppView> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
       theme: squadTheme(),
       darkTheme: squadTheme(isDarkMode: true),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English, no country code
+      ],
       home: const HomeView(),
     );
   }
